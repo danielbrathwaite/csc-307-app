f3119a9fafef6f59aa6ee161ef98f662fb03c063
Daniel Brathwaite <dbrathwaite.ai@gmail.com>
2024-04-09 21:47:14 -0700

    Added react hooks + state

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -9,9 +9,21 @@ function MyApp() {
     } // the rest of the data
   ]);
 
+  function removeOneCharacter(index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index;
+    });
+    setCharacters(updated);
+  }
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table
+        characterData={characters}
+        removeCharacter={removeOneCharacter}
+        />
     </div>
   );
 }
+
+export default MyApp;
