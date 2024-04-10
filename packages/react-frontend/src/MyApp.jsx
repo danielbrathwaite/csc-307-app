346eadc0b0987e87cfa6947dcb2774119f83db14
Daniel Brathwaite <dbrathwaite.ai@gmail.com>
2024-04-09 22:15:44 -0700

    Form submit

--- packages/react-frontend/src/MyApp.jsx
+++ packages/react-frontend/src/MyApp.jsx
@@ -3,7 +3,7 @@ import Table from "./Table";
 import Form from "./Form";
 
 function MyApp() {
-  const [characters, setCharacters] = useState([{}]);
+  const [characters, setCharacters] = useState([]);
 
   function removeOneCharacter(index) {
     const updated = characters.filter((character, i) => {
@@ -12,13 +12,17 @@ function MyApp() {
     setCharacters(updated);
   }
 
+  function updateList(person) {
+    setCharacters([...characters, person]);
+  }
+
   return (
     <div className="container">
       <Table
         characterData={characters}
         removeCharacter={removeOneCharacter}
         />
-        <Form />
+        <Form handleSubmit={updateList} />
     </div>
   );
 }
